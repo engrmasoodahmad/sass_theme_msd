03666b761e3eabd640c27bbf2ed7c2088480883e
Masood Ahmad <masood@drupak.com>
2023-08-28 14:30:48 +0500

    Slick Slider has been added and a commented code is left there for help

---
+++ js/sliders.js
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * All Sliders of the site will go here.
+ */
+(function ($, Drupal) {
+	Drupal.behaviors.sliders = {
+		attach(context, settings) {
+			// JUST REUSE THIS CODE FOR SLIDER
+			// $(".hero-slider").not(".slick-initialized").slick({
+			// 	infinite: true,
+			// 	speed: 300,
+			// 	slidesToShow: 1,
+			// 	autoplay: true,
+			// 	autoplaySpeed: 3000,
+			// 	pauseOnHover: true,
+			// 	arrows: false,
+			// });
+		},
+	};
+})(jQuery, Drupal);
